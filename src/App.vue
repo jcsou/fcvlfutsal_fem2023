c480495a3ebf8a903ce2a8e050306ea8a140ddcd
Jc Souvignet <jcsovignet@gmail.com>
2022-12-08 23:55:11 +0100

    Init dev

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+
+    <!-- Start of Navigation -->
+    <nav>
+      <!-- Start of app toolbar -->
+      <v-toolbar app>
+        <v-toolbar-side-icon @click.stop="drawer = !drawer" class="hidden-md-and-up">
+        </v-toolbar-side-icon>
+
+        <v-toolbar-title class="headline text-uppercase">
+            Vuetify Demo
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn flat to="/" exact>Home</v-btn>
+          <v-btn flat to="/about">About</v-btn>
+          <v-btn flat to="/test">Test</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <!-- End of app toolbar -->
+
+      <!-- Start of mobile side menu -->
+      <v-navigation-drawer app v-model="drawer" right>
+        <!-- Menu title -->
+        <v-toolbar flat>
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-title class="title">Menu</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-toolbar>
+        <v-divider></v-divider>
+        <!-- Menu Links -->
+        <v-list>
+          <v-list-tile to="/" exact>
+            <v-list-tile-content>Home</v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile to="/about">
+            <v-list-tile-content>About</v-list-tile-content>
+          </v-list-tile>
+        <v-list-tile to="/test">
+          <v-list-tile-content>Test</v-list-tile-content>
+        </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <!-- End of mobile side menu -->
+    </nav>
+    <!-- End of Navigation -->
+
+    <v-content>
+      <!-- Display view pages here based on route -->
+      <router-view></router-view>
+    </v-content>
+
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    data() {
+      return {
+        drawer: false // Hide mobile side menu by default
+      };
+    }
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-container>
+    <v-layout>
+      <h1>Home page</h1>
+    </v-layout>
+  </v-container>
+</template>
